e5d5fce90860f982e3eac29fa3572ee060ec6430
max <68344959+codewmax@users.noreply.github.com>
2023-10-07 09:26:35 +0200

    3

--- boilersite2.js
+++ boilersite2.js
@@ -2,9 +2,7 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
 document.getElementsByTagName('head')[0].appendChild(script);
 
-$(document).on("mouseover",function(e){
-  console.log("$(e.target).attr('id')"); // i just retrieved the id for a demo
-});
+
 
 
 var $code = document.getElementById('code');
