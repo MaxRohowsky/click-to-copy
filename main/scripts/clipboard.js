bf7df0193f1d50585a4ed8c541dcb3573f1e05b8
max <68344959+codewmax@users.noreply.github.com>
2024-02-02 17:06:10 +0100

    Update clipboard UI in main/scripts/clipboard.js

--- main/scripts/clipboard.js
+++ main/scripts/clipboard.js
@@ -1,18 +1,54 @@
 class Clipboard {
     constructor() {
-        this.copiedText = [];
-        this.copiedUrls = [];
-        this.copiedColors = [];
-        this.lastTextIndex = 0;
-        this.lastUrlIndex = 0;
-        this.lastColorIndex = 0;
-        this.container = $('<div>').attr('id', 'clipboard_container');
+        this.copiedText           = [];
+        this.copiedUrls           = [];
+        this.copiedColors         = [];
+        this.lastTextIndex        = 0;
+        this.lastUrlIndex         = 0;
+        this.lastColorIndex       = 0;
+        this.clipboard            = $('<div>').attr('id', 'clipboard');
+        this.clipboardContainer   = $('<div>').attr('id', 'clipboard_container');
+        this.clipboardTop         = $('<div>').attr('id', 'clipboard_top');
+        this.clipboardSub         = $('<div>').attr('id', 'clipboard_sub');
+        this.clipboardMid         = $('<div>').attr('id', 'clipboard_mid');
+        this.clipboardItemHeader  = $('<div>').attr('id', 'clipboard_item_header').text('Clipboard');
+        this.clipboardItems       = $('<div>').attr('id', 'clipboard_items')
+        this.clipboardEnd         = $('<div>').attr('id', 'clipboard_end');
+        this.clipboardCopyButton  = $('<button>').attr('id', 'clipboard_copy_button').text('Copy');
+        this.clipboardClearButton = $('<button>').attr('id', 'clipboard_clear_button');
         this.BuildClipboard();
+
     }
 
     BuildClipboard() {
-        $('body').append(this.container);
-        this.container.draggable();
+        const trashIcon = $('<img>')
+            .addClass('clipboard_trash_icon')
+            .attr('src', `chrome-extension://laonhdndhpeoachehnobbcjdcnnhlioe/assets/trash-icon.svg`);
+
+        const closeIcon = $('<img>')
+            .addClass('clipboard_close_icon')
+            .attr('src', `chrome-extension://laonhdndhpeoachehnobbcjdcnnhlioe/assets/close-classic-icon.svg`);
+
+
+        this.clipboard.append(this.clipboardContainer);
+
+        this.clipboardContainer.append(this.clipboardTop);
+        this.clipboardTop.append(closeIcon);
+
+        this.clipboardContainer.append(this.clipboardSub);
+
+        this.clipboardContainer.append(this.clipboardMid);
+        this.clipboardMid.append(this.clipboardItemHeader);
+        this.clipboardMid.append(this.clipboardItems);
+
+        this.clipboardContainer.append(this.clipboardEnd);
+        this.clipboardEnd.append(this.clipboardCopyButton);
+        this.clipboardEnd.append(this.clipboardClearButton);
+        this.clipboardClearButton.append(trashIcon);
+
+
+        $('body').append(this.clipboard);
+        this.clipboard.draggable();
 
     }
 
@@ -20,19 +56,21 @@ class Clipboard {
         for (let i = this.lastTextIndex; i < this.copiedText.length; i++) {
             let div = $('<div>').text(this.copiedText[i])
             .attr('class', 'clipboard_item clipboard_text');
-            this.container.append(div);
+            this.clipboardItems.append(div);
         }
         this.lastTextIndex = this.copiedText.length;
 
         for (let i = this.lastUrlIndex; i < this.copiedUrls.length; i++) {
-            let div = $('<div>').text(this.copiedUrls[i]).attr('class', 'clipboard_item clipboard_url');
-            this.container.append(div);
+            let div = $('<div>').text(this.copiedUrls[i])
+            .attr('class', 'clipboard_item clipboard_url');
+            this.clipboardItems.append(div);
         }
         this.lastUrlIndex = this.copiedUrls.length;
 
         for (let i = this.lastColorIndex; i < this.copiedColors.length; i++) {
-            let div = $('<div>').text(this.copiedColors[i]).attr('class', 'clipboard_item clipboard_color');
-            this.container.append(div);
+            let div = $('<div>').text(this.copiedColors[i])
+            .attr('class', 'clipboard_item clipboard_color');
+            this.clipboardItems.append(div);
         }
         this.lastColorIndex = this.copiedColors.length;
     }
@@ -40,16 +78,16 @@ class Clipboard {
 
     add(val, list) {
         switch (list) {
-            case 'text':
+            case 'text': 
                 this.copiedText.push(val);
                 break;
-            case 'urls':
+            case 'urls': 
                 this.copiedUrls.push(val);
                 break;
-            case 'colors':
+            case 'colors': 
                 this.copiedColors.push(val);
                 break;
-            default:
+            default: 
                 throw new Error('Invalid list specified');
         }
         this.refresh();
