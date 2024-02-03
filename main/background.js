72cc72d5f49198136a2e6c98d653970e68714acc
max <68344959+codewmax@users.noreply.github.com>
2024-02-03 16:18:47 +0100

    Add clipboard CSS style

--- main/background.js
+++ main/background.js
@@ -3,5 +3,6 @@ chrome.action.onClicked.addListener(function(tab)
 
 	chrome.tabs.insertCSS(tab.id, {file:'style/viewer.css'});
 	chrome.tabs.insertCSS(tab.id, {file:'style/appmenu.css'});
+	chrome.tabs.insertCSS(tab.id, {file:'style/clipboard.css'});
 
 });
