2f999ba7ddc3568c362346729e867d7fe16235bb
max <68344959+codewmax@users.noreply.github.com>
2024-02-26 20:02:03 +0100

    Delete unnecessary files and code

--- test/content.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("mouseover", (event) => {
-    const target = event.target;
-    const styles = window.getComputedStyle(target);
-    const cssText = Array.from(styles)
-      .map((prop) => `${prop}: ${styles.getPropertyValue(prop)}`)
-      .join("<br>");
-    chrome.runtime.sendMessage({ cssText });
-  });
--- test/script.js
+++
@@ -1,5 +0,0 @@
-// Handle clicking on the extension icon
-chrome.browserAction.onClicked.addListener(function (tab) {
-  // Open popup.html in a new tab
-  chrome.tabs.create({ url: "popup.html" });
-});
